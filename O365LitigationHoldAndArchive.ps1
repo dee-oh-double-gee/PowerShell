7514c1e51c806bde680a344260322d15b8ed21f2
dee-oh-double-gee <beaugraham@gmail.com>
2017-10-30 11:14:05 -0600

    Adding O365 Litigation and Archive

---
+++ O365LitigationHoldAndArchive.ps1
@@ -0,0 +1,21 @@
+﻿## Connect to O365
+
+$credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+
+## Enable Litigation Hold and Archiving for any mailbox without those things
+
+Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-Mailbox -LitigationHoldEnabled $true -LitigationHoldDuration Unlimited
+
+Get-Mailbox -Filter {ArchiveStatus -Eq "None" -AND RecipientTypeDetails -eq "UserMailbox"} | Enable-Mailbox -Archive
+
+
+
+
+## Disconnect
+
+Remove-PSSession $exchangeSession
