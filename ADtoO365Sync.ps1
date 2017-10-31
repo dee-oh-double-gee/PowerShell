ec5f242e0b8c474c513974b4efe4e6fab5950fd9
dee-oh-double-gee <beaugraham@gmail.com>
2017-10-31 13:02:19 -0600

    Add Force Update AD --> O365
    Run this if you want to force an update to O365 instead of waiting for the half hour to be up for the normal sync interval

---
+++ ADtoO365Sync.ps1
@@ -0,0 +1,14 @@
+﻿Write-Host "Initializing Azure AD Delta Sync..." -ForegroundColor Yellow
+
+invoke-command -ComputerName awcc-ut1-us <# Change to your server that does the sync #> -scriptblock {Start-ADSyncSyncCycle -PolicyType Delta}
+
+#Wait 10 seconds for the sync connector to wake up.
+Start-Sleep -Seconds 10
+
+#Display a progress indicator and hold up the rest of the script while the sync completes.
+While(invoke-command -ComputerName awcc-ut1-us <# Change to your server that does the sync #> -scriptblock {Get-ADSyncConnectorRunStatus}){
+    Write-Host "." -NoNewline
+    Start-Sleep -Seconds 10
+}
+
+Write-Host " | Complete!" -ForegroundColor Green
