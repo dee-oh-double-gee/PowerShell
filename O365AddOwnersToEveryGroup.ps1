5bbfccc8359c294bb3ef7f07c5b80d2e07d4460f
dee-oh-double-gee <beaugraham@gmail.com>
2017-10-31 16:44:33 -0600

    Added Connect to O365

---
+++ O365AddOwnersToEveryGroup.ps1
@@ -0,0 +1,15 @@
+﻿## Connect to O365
+
+$credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+## Who do you want as an owner of every group
+
+$Owner = read-host "What is the email of the person you want to be an owner over every group?"
+
+
+
+Get-DistributionGroup |Set-DistributionGroup -ManagedBy @{Add="$owner"} –BypassSecurityGroupManagerCheck
